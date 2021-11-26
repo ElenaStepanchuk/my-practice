8b72bd4fb735e6c0e9afd42c53cf73daadae588e
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-26 20:54:18 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './sass/main.scss';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import simpleLightbox from 'simplelightbox';
 
 // Для gallery-simpleLightbox.html
 
