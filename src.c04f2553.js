feda4cca253e6abb2a7a7c9a2a8fdb995b1dd203
ElenaStepanchuk <83465804+ElenaStepanchuk@users.noreply.github.com>
2021-12-01 19:06:47 +0000

    Deploying to gh-pages from @ ElenaStepanchuk/my-practice@b89c89b3c63a408434bc95cd64e3418c8952cfc8 🚀

--- src.c04f2553.js
+++ src.c04f2553.js
@@ -6,6 +6,6 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"kz9M":[function(require,module,exports) {
 
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");var t=i(require("simplelightbox"));function i(t){return t&&t.__esModule?t:{default:t}}require("simplelightbox/dist/simple-lightbox.min.css");const o=new t.default(".more-photos-simplightbox a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,close:!0}),a=new t.default(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,close:!0});
+"use strict";require("./sass/main.scss");var t=i(require("simplelightbox"));function i(t){return t&&t.__esModule?t:{default:t}}require("simplelightbox/dist/simple-lightbox.min.css");const o=new t.default(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,close:!0}),a=new t.default(".more-photos-simplightbox a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,close:!0});
 },{"./sass/main.scss":"clu1","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["Focm"], null)
-//# sourceMappingURL=/my-practice/src.6a73acac.js.map
+//# sourceMappingURL=/my-practice/src.c04f2553.js.map
