9a668b0c2013f5afb532f0b7586a63c68d895c68
ElenaStepanchuk <podolyak365@gmail.com>
2021-12-01 20:27:52 +0200

    lightbox

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Для gallery-simpleLightbox.html
 
-const lightbox = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.more-photos-simplightbox a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    nav: true,
+    close: true,
+});
+
+const lightbox1 = new SimpleLightbox('.gallery a', {
     captions: true,
     captionsData: 'alt',
     captionPosition: 'bottom',
